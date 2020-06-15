fa6af48e964150f6d7833c64efc7d12ec2278c4b
ln-ning <3165837730@qq.com>
2020-06-15 12:37:03 +0800

    默认

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>1456456456456464</h1>
+    <h1>14564564456464</h1>
   </div>
 </template>
---
+++ src/views/login.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    登录页
+</div>
+</template>
+
+<script>
+export default {
+  components:{},
+  props:{},
+  data(){
+    return {
+    }
+  },
+}
+</script>
+<style scoped>
+</style>
