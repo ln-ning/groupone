eb2774325344bc6c80a6bd62da3ca42c580e4d35
ln-ning <3165837730@qq.com>
2020-06-15 10:19:53 +0800

    默认配置

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+//饿了么框架    
+import ElementUI from 'element-ui';
+//import '../node_modules/element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
