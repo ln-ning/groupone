3a713f5bae28550c145e29ce2cd8f12f676f91dc
ln-ning <3165837730@qq.com>
2020-06-15 12:21:49 +0800

    默认

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="about">
-<<<<<<< HEAD
     <h1>1456456456456464</h1>
-=======
-    <h1>This is an about page</h1>
->>>>>>> e68cbcf8a85e1a1c49f2fe1e1d2569e288b2b239
   </div>
 </template>
