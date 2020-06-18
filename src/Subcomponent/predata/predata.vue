0001ccf31b61898c79cd05690cd9bcaa36e75d81
yingguang123 <1733630402@qq.com>
2020-06-18 08:01:01 +0800

    '6月18日8点提交'

--- src/Subcomponent/predata/predata.vue
+++ src/Subcomponent/predata/predata.vue
@@ -1,18 +1,114 @@
 <template>
-<div>
-    个人资料
-</div>
+  <div class="big">
+    <div class="bgColor">
+      <header>设置我的资料</header>
+      <el-form :model="ruleForm" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="图片">
+          <el-upload class="avatar-uploader" action="https://jsonplaceholder.typicode.com/posts/"
+            :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+            <img v-if="ruleForm.imageUrl" :src="ruleForm.imageUrl" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-form-item>
+        <el-form-item label="姓名" prop="name">
+          <el-input style="width: 26.8%;" v-model="ruleForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="性别" prop="sex">
+          <el-input style="width: 26.8%;" v-model="ruleForm.sex"></el-input>
+        </el-form-item>
+        <el-form-item label="年龄" prop="age">
+          <el-input style="width: 26.8%;" v-model="ruleForm.age"></el-input>
+        </el-form-item>
+        <el-form-item label="身份证号" prop="idCard">
+          <el-input style="width: 26.8%;" v-model="ruleForm.idCard"></el-input>
+        </el-form-item>
+        <el-form-item label="民族" prop="nation">
+          <el-input style="width: 26.8%;" v-model="ruleForm.nation"></el-input>
+        </el-form-item>
+        <el-form-item label="学历" prop="education">
+          <el-select v-model="ruleForm.education" placeholder="——请选择——">
+            <el-option label="——请选择——" value=""></el-option>
+            <el-option label="初中" value="初中"></el-option>
+            <el-option label="高中" value="高中"></el-option>
+            <el-option label="专科" value="专科"></el-option>
+            <el-option label="本科" value="本科"></el-option>
+            <el-option label="研究生" value="研究生"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="所属部门" prop="department">
+          <el-input style="width: 26.8%;" v-model="ruleForm.department"></el-input>
+        </el-form-item>
+        <el-form-item label="职位" prop="post">
+          <el-input style="width: 26.8%;" v-model="ruleForm.post"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" prop="cellphone">
+          <el-input style="width: 26.8%;" v-model="ruleForm.cellphone" placeholder="请输入电话"></el-input>
+        </el-form-item>
+        <el-form-item label="地址" prop="address">
+          <el-input style="width: 26.8%;" v-model="ruleForm.address" placeholder="请输入地址"></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
+    <el-row>
+      <el-button size="mini" @click="submitForm('ruleForm')">修改</el-button>
+      <el-button size="mini">取消</el-button>
+    </el-row>
+  </div>
 </template>
 
 <script>
-export default {
-  components:{},
-  props:{},
-  data(){
-    return {
-    }
-  },
-}
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {
+        ruleForm: {
+          imageUrl:'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+          name: '张三',
+          sex: '男',
+          age: '20',
+          idCard: '410181200002255549',
+          nation: '汉 ',
+          education: '专科',
+          department: '人资',
+          post: '经理',
+          cellphone: '11111111111',
+          address: '河南'
+        },
+      }
+    },
+    methods: {
+      // 提交
+      submitForm(formName) {
+        console.log(this.ruleForm);
+        // 弹框
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('修改成功');
+          } else {
+            return false;
+          }
+        });
+      },
+      // 图片上传
+      handleAvatarSuccess(res, file) {
+        this.ruleForm.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
+      }
+    },
+  }
 </script>
 <style scoped>
+  @import './predata.css'
 </style>
